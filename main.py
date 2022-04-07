1a4fbf3322e750c0e2c0a3ad6f9f6b4fdd294b8f
fsousa <fsousa@umich.edu>
2022-04-07 19:45:02 -0400

    Missing visualizations

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ import re
 import os
 import csv
 import unittest
+import io
+import sys
 import json
+
+# TODO:
+'''
+- 25 or fewer restriction
+- Create at least 2 visualizations of the calculated data
+'''
+
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -34,11 +43,19 @@ def retrieveDictfromData(fileName):
     return data
 
 def CovidDatatoDB(data, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Covid (date INTEGER PRIMARY KEY, positive INTEGER, positive_inc INTEGER, hospitalized_cur INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Covid (date DATE PRIMARY KEY, month INTEGER, positive INTEGER, positive_inc INTEGER, hospitalized_cur INTEGER)")
     conn.commit()
-    for day in data:
-        cur.execute("INSERT OR IGNORE INTO Covid (date, positive, positive_inc, hospitalized_cur) VALUES (?,?,?,?)",(day['date'],day['positive'],day['positiveIncrease'],day['hospitalizedCurrently']))
+    for row in data:
+        date = str(row['date'])
+        day = date[-2:]
+        month = date[4:6]
+        year = date[0:4]
+        if year != '2020':
+            continue
+        date = year + '-' + month + '-' + day
+        cur.execute("INSERT OR IGNORE INTO Covid (date, month, positive, positive_inc, hospitalized_cur) VALUES (?,?,?,?,?)",(date, month, row['positive'],row['positiveIncrease'],row['hospitalizedCurrently']))
     conn.commit()
+    return
 
 
 def retrieveData(filename):
@@ -70,21 +87,28 @@ def abbottToDB(data, cur, conn):
         month = int(date[1])
         cur.execute("INSERT OR IGNORE INTO Abbott (date, month, adjClose) VALUES (?, ?, ?)",(row[1], month, row[0]))
     conn.commit()
+    return
 
 def deltaToDB(data, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Delta (date DATE PRIMARY KEY, adjClose FLOAT(2))")
+    cur.execute("CREATE TABLE IF NOT EXISTS Delta (date DATE PRIMARY KEY, month INTEGER, adjClose FLOAT(2))")
     conn.commit()
-    for day in data:
-        cur.execute("INSERT OR IGNORE INTO Delta (date, adjClose) VALUES (?, ?)",(day[1], day[0]))
+    for row in data:
+        date = row[1].split('-')
+        month = int(date[1])
+        cur.execute("INSERT OR IGNORE INTO Delta (date, month, adjClose) VALUES (?, ?, ?)",(row[1], month, row[0]))
     conn.commit()
+    return
 
 def SPToDB(data, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS SP500 (date DATE PRIMARY KEY, adjClose FLOAT(2))")
+    cur.execute("CREATE TABLE IF NOT EXISTS SP500 (date DATE PRIMARY KEY, month INTEGER, adjClose FLOAT(2))")
     conn.commit()
-    for day in data:
-        cur.execute("INSERT OR IGNORE INTO SP500 (date, adjClose) VALUES (?, ?)",(day[1], day[0]))
+    for row in data:
+        date = row[1].split('-')
+        month = int(date[1])
+        stock = row[0].replace(',', "")
+        cur.execute("INSERT OR IGNORE INTO SP500 (date, month, adjClose) VALUES (?, ?, ?)",(row[1], month, stock))
     conn.commit()
-
+    return
 
 def monthNumber(month):
     if month == 'Jan': 
@@ -113,7 +137,20 @@ def monthNumber(month):
         return '12'
 
 def SPCovidPlot(cur, conn):
-    pass
+    cur.execute(
+        '''
+        SELECT SP500.adjClose, SP500.date, Covid.positive
+        FROM SP500
+        JOIN Covid ON SP500.date = Covid.date
+        '''
+    )
+    res = cur.fetchall()
+    ySP = [mon[0] for mon in res]
+    yCovid = [mon[2] for mon in res]
+    x = [mon[1] for mon in res]
+
+    # TODO: Create line chart with x and y
+    return
 
 def abbottCovidPlot(cur, conn):
     cur.execute(
@@ -124,23 +161,65 @@ def abbottCovidPlot(cur, conn):
         '''
     )
     res = cur.fetchall()
-    x = [mon[0] for mon in res]
-    y = [mon[1] for mon in res]
-    pass
+    yAb = [mon[0] for mon in res]
+    yCovid = [mon[2] for mon in res]
+    x = [mon[1] for mon in res]
 
-def AbbottMonth(cur, conn):
+    # TODO: Create line chart with x and y
+    return
+
+def deltaCovidPlot(cur, conn):
     cur.execute(
         '''
-        SELECT AVG(Abbott.adjClose), Abbott.month
+        SELECT Delta.adjClose, Delta.date, Covid.positive
+        FROM Delta
+        JOIN Covid ON Delta.date = Covid.date
+        '''
+    )
+    res = cur.fetchall()
+    yDelta = [mon[0] for mon in res]
+    yCovid = [mon[2] for mon in res]
+    x = [mon[1] for mon in res]
+
+    # TODO: Create line chart with x and y
+    return
+
+def Month(cur, conn):
+    cur.execute(
+        '''
+        SELECT Abbott.month, AVG(Covid.positive), AVG(Abbott.adjClose), AVG(Delta.adjClose), AVG(SP500.adjClose)
         FROM Abbott
+        JOIN Covid JOIN Delta JOIN SP500 ON Covid.month = Abbott.month AND Delta.month = Abbott.month AND SP500.month = Abbott.month 
         GROUP BY Abbott.month
         '''
     )
     res = cur.fetchall()
 
-def deltaCovidPlot():
-    delta = retrieveData("Delta.html")
-    pass
+    fout = open("Monthly_Data.csv", "w")
+    writer = csv.writer(fout)
+
+    headerRow = ["Month", "Covid-19 Daily Average", "Abbott Average Closing Price", "Delta Average Closing Price", "S&P500 Average Closing Price"]
+    writer.writerow(headerRow)
+    for month in res:
+        cases = round(month[1], 2)
+        abbott = round(month[2], 2)
+        delta = round(month[3], 2)
+        sp = round(month[4], 2)
+        writer.writerow([month[0], cases, abbott, delta, sp])
+    
+    fout.close()
+    return
+
+def main():
+    cur, conn = setUpDatabase("Data.db")
+    CovidDatatoDB(retrieveDictfromData('Covid.json'), cur, conn)
+    abbottToDB(retrieveData("Abbott.html"), cur, conn)
+    deltaToDB(retrieveData("Delta.html"), cur, conn)
+    SPToDB(retrieveData("S&P.html"), cur, conn)
+    Month(cur, conn)
+    SPCovidPlot(cur, conn)
+    abbottCovidPlot(cur, conn)
+    deltaCovidPlot(cur, conn)
 
 
 class TestCases(unittest.TestCase):
@@ -161,14 +240,14 @@ class TestCases(unittest.TestCase):
         deltaToDB(self.dataDelta, self.cur, self.conn)
 
         self.dataSP = retrieveData("S&P.html")
-        SPToDB(self.dataDelta, self.cur, self.conn)
+        SPToDB(self.dataSP, self.cur, self.conn)
 
     def testPlots(self):
         self.cur, self.conn = setUpDatabase('Data.db')
         abbottCovidPlot(self.cur, self.conn)
+        Month(self.cur, self.conn)
         
 
-
-
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    main()
+    # unittest.main(verbosity=2)
